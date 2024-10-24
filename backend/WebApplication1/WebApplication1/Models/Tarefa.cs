7d870a5cdacaad8427cf9b9cad503ffb86710acb
hpsouza1 <hpsouza1@stefanini.com>
2024-10-24 10:23:48 -0300

    excluindo back

--- backend/WebApplication1/WebApplication1/Models/Tarefa.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class Tarefa
-    {
-        public int Id { get; set; } 
-        public string Titulo { get; set; } = string.Empty; 
-        public string Descricao { get; set; } = string.Empty; 
-    }
-}
